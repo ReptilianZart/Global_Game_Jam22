65447de3405778fd069d3eaf340f5dcf3162b29c
AngeloAI <oliver.chalk@btinternet.com>
2022-01-28 23:17:50 +0000

    made changes

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ g.init()
 
 
 while not gv.EXITGAME:
-    g.draw(spr.whiteCat, 400, 400)
+    g.draw(spr.background, 0, 0)
+    g.draw(spr.whiteCat, 800, 400)
+    g.draw(spr.blackCat, 200, 200)
     g.run()
     g.update()
--- sprites.py
+++ sprites.py
@@ -1,5 +1,7 @@
 import pygame
 
-whiteCat = pygame.image.load("spriteFolder\whiteCat.png")
+whiteCat = pygame.image.load("spriteFolder/whiteCat.png")
 
-blackCat = pygame.image.load("spriteFolder\blackCat.png")
+blackCat = pygame.image.load("spriteFolder/blackCat.png")
+
+background = pygame.image.load("spriteFolder/galaxy.png")
