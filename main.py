76a6e61e9e8437afea7a15e6c087dabdd3aa2774
AngeloAI <90303864+AngeloAI@users.noreply.github.com>
2022-01-28 17:44:54 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import projectile
+import sprites
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+# Initialise the pygame
+pygame.init()
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+# Title and Icon
+pygame.display.set_caption("Duality")
+#icon = pygame.image.load("Icon.png")
