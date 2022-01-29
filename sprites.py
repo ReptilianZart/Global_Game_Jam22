a5d3415629d3242e9590cc5e852e76c0c66980a8
ReptilianZart <andreasjjhome@gmail.com>
2022-01-29 01:47:31 +0000

    font

--- sprites.py
+++ sprites.py
@@ -5,3 +5,17 @@ whiteCat = pygame.image.load("spriteFolder/whiteCat3.png")
 blackCat = pygame.image.load("spriteFolder/blackCat.png")
 
 background = pygame.image.load("spriteFolder/galaxy.png")
+
+# MAIN MENU
+pygame.font.init()
+
+logo = pygame.image.load("spriteFolder/title.png")
+title = pygame.image.load("spriteFolder/title.png")
+
+arcadeFont1 = pygame.font.Font("fonts/arcade1.ttf", 22)
+keyprompt1 = arcadeFont1.render("Press any key to continue", True, (255,255,255))
+arcadeFont2 = pygame.font.Font("fonts/arcade1.ttf", 24)
+keyprompt2 = arcadeFont2.render("Press any key to continue", True, (255,255,255))
+
+creditFont = pygame.font.Font("fonts/arcade2.ttf", 15)
+credit = creditFont.render("Created by ", True, (255,255,255))
